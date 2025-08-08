4ac5900231a1396ac2547046207b518c5165514b
abdullahomar321 <abdullahomer.0504@gmail.com>
2025-08-08 21:07:46 +0500

    Create security.dart

---
+++ lib/security.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class security extends StatelessWidget {
+  const security({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
